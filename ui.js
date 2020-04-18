b062ad3dbab22658e60db3e4b70758a02e69a30b
Jeniffer Carvalho <jenicarvalho@live.com>
2020-04-18 19:28:54 -0300

    adicionaro querySelector diretamente

--- ui.js
+++ ui.js
@@ -1,32 +1,27 @@
 class UI {
-    constructor() {
-        this.copyright = document.querySelector('.copyright');
-        this.comics = document.querySelector('.comics ul');
-    }
-
-    paint(marvel) { 
+    paint(marvel) {
         //show marvel rights
-        this.copyright.innerHTML =  `
+        document.querySelector('.copyright').innerHTML = `
             <h1>${marvel.copyright}</h1>
             <p>${marvel.attributionHTML}</p>
         `;
 
         //only comics from api
-        const comics = marvel.data.results; 
+        const comics = marvel.data.results;
         //show marvel comics
         let output = '';
 
         comics.forEach(comic => {
             output +=
-             `
+                `
                 <li>
 									<div class='description'><p>${comic.description}</p></div>
 									<img src="${comic.thumbnail.path}.${comic.thumbnail.extension}">
 									<h3>${comic.title}</h3>
                 </li>
             `;
-            
-				}); 
-        this.comics.innerHTML = output;
+
+        });
+        document.querySelector('.comics ul').innerHTML = output;
     }
 }
